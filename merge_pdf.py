7f538fe6a41532520758ee95f57affd8d7e7acb7
danyeric123 <dnagarpowers@gmail.com>
2021-10-10 18:00:31 -0400

    can now select files using gui

--- merge_pdf.py
+++ merge_pdf.py
@@ -1,11 +1,17 @@
 from PyPDF2 import PdfFileMerger
 import glob
+from tkinter import Entry, Label, StringVar, Tk
+from tkinter.filedialog import askopenfilename, askopenfilenames
 
-files = glob.glob('pdfs/*.pdf')
+Tk().withdraw()
+filenames = askopenfilenames(title='Choose pdfs to merge',filetypes=[("PDF files","*.pdf")])
+
 merger = PdfFileMerger()
 
-for file_ in files:
+for file_ in filenames:
   merger.append(file_)
+  
 
-merger.write('merged.pdf')
+merger.write(f'merged.pdf')
+
 merger.close()
