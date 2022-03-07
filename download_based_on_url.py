f409e766b6c7c4731cc61c2f2063c5b8a6f1e85c
danyeric123 <dnagarpowers@gmail.com>
2022-03-07 12:41:59 -0500

    format by black

--- download_based_on_url.py
+++ download_based_on_url.py
@@ -1,14 +1,15 @@
 import requests
 
-def download_file(url : str):
-  r = requests.get(url)
-  filename = url.split('/')[-1]
-  
-  with open(filename,'wb') as f:
-    f.write(r.content)
-    
 
-with open('urls_to_download_from.txt') as txt:
-  urls = txt.read().splitlines()
-  for url in urls:
-    download_file(url)
+def download_file(url: str):
+    r = requests.get(url)
+    filename = url.split("/")[-1]
+
+    with open(filename, "wb") as f:
+        f.write(r.content)
+
+
+with open("urls_to_download_from.txt") as txt:
+    urls = txt.read().splitlines()
+    for url in urls:
+        download_file(url)
--- send_sms.py
+++ send_sms.py
@@ -1,4 +1,3 @@
-
 from twilio.rest import Client
 import dotenv
 import os
@@ -6,18 +5,19 @@ import os
 dotenv.load_dotenv()
 
 # Your Account SID from twilio.com/console
-account_sid = os.environ.get('ACCOUNT_SID')
+account_sid = os.environ.get("ACCOUNT_SID")
 # Your Auth Token from twilio.com/console
-auth_token  = os.environ.get('AUTH_TOKEN')
+auth_token = os.environ.get("AUTH_TOKEN")
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+16466258268", 
+    to="+16466258268",
     from_="+19175355452",
     body="""
     Hello from my Python script!
     Love David
-    """)
+    """,
+)
 
 print(message.sid)
